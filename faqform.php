749b4aa6941c4e4eb5172c4f9cab73c609519677
leonardo <xxrastaxx55@gmail.com>
2022-12-11 09:47:36 -0300

    crud ready

--- faqform.php
+++ faqform.php
@@ -1,14 +1,26 @@
 <?php 
 if (!isset($_SESSION)) { session_start(); }
 require("fnc/funcionesUsuario.php");
+include "config/config.php";
 
 
 $user = $_SESSION["username"];
+
+$search = $_POST["search"];
 
 if (empty($user)) {
     $destino = "error.html";
     header("location: $destino");	
 }
+
+// select all faqs where search = Post search
+
+$stmt = $conn->prepare("SELECT *  FROM faqs WHERE question LIKE :search OR categoria LIKE :search OR priority LIKE :search");
+$search = '%'.$_POST['search'].'%';
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$faqs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 
 
@@ -64,10 +76,10 @@ if (empty($user)) {
 					<!-- <?php foreach ($faqs as $faq): ?> -->
 						<tr>
 							
-							<td><?php  ?></td>
-							<td><?php ?></td>
-							<td><?php  ?></td>
-							<td><?php  ?></td>
+							<td><?php echo $faq["question"]  ?></td>
+							<td><?php echo $faq["answer"]; ?></td>
+							<td><?php echo $faq["categoria"]; ?></td>
+							<td><?php echo $faq["priority"]; ?></td>
 							<td>
 								<!-- edit button -->
 								<a href="faqedit.php?id=<?php echo $faq['id']; ?>" class="btn btn-warning btn-sm">
@@ -75,6 +87,11 @@ if (empty($user)) {
 								</a>
 
 								<!-- delete form -->
+
+								<form method="POST" action="faqdel.php" onsubmit="return confirm('Estas seguro de eliminar esta FAQ ?');">
+									<input type="hidden" name="id" value="<?php echo $faq['id']; ?>" required />
+									<input type="submit" value="Eliminar" class="btn btn-danger btn-sm" />
+								</form>
 								
 							</td>
 						</tr>
